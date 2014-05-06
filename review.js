f94048de0e82a5f67cdb8ca854d84fb5c51c31ce
jefferyharlowbrewer@gmail.com <>
2014-05-06 07:59:13 +0000

    solution

---
+++ review.js
@@ -0,0 +1,33 @@
+var baseUrl = "https://jeffb531-service.codenvy.ctof.intuit.com/api";
+
+$(document).ready(function() {
+  $.ajax({
+      url:baseUrl+"/v1/reviews/review/"+location.search.slice(1),
+      contentType:"application/json; charset=utf-8",
+      dataType: 'json',
+      type: 'GET'
+    }).done(function(data) {
+      $('#customerName').text(data.customerName)
+      $('#customerEmail').text(data.customerEmail);
+      $('#review').text(data.content);
+    });
+  
+  $('#retweet').click(function() {
+    $("#message").text("Posting to twitter...");
+    $.ajax({
+      url:baseUrl+"/v1/blabber",
+      contentType:"application/json; charset=utf-8",
+      dataType: 'json',
+      type: 'POST',
+      data: $("#review").val()
+    }).done(function(data) {
+      $("#message").text("Posted");
+    }).fail(function( jqXHR, textStatus, errorThrown) {
+      if (jqXHR.status != 200) {
+        $("#message").text("Error - it failed");
+      } else {
+      $("#message").text("Posted");        
+      }
+    });
+  });
+});
